2c94f0e1830231053549070a58e1ac0b0ea86580
yossisilb <yossisilb@gmail.com>
2015-12-14 23:39:13 +0200

    Changed the socket type to the correct Socket

--- src/UDP Client/mainUDPClient.cpp
+++ src/UDP Client/mainUDPClient.cpp
@@ -1,22 +1,20 @@
+#include <string>
 #include "UDPClient.h"
 
+using namespace std;
 #define IP_ADDRESS "127.0.0.1"
-#define SERVER_PORT 3000
+#define SERVER_PORT 5678
 
 int main() {
     // the string being sent
-    char data[] = "Do you want to build a snowman?";
+    string data = "1 xyz Inception 148 2010 8.8 This is the description";
     //the size of string
     int data_len = sizeof(data);
 
     UDPClient* client = new UDPClient;
-
-    client->newSocket(SOCK_STREAM);
-    client->connectUDP(SERVER_PORT, IP_ADDRESS);
-
-    client->sendTo(data, data_len);
-
+    client->newSocket(SOCK_DGRAM);
+    client->sendTo(IP_ADDRESS, SERVER_PORT, data);
     client->receiveFrom();
-
+    sleep(20);
     client->closeConnection();
 }
