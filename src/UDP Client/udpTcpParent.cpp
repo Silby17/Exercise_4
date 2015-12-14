900a657c3591a6816aa70c139dd7b90fd067a4a6
yossisilb <yossisilb@gmail.com>
2015-12-14 21:58:41 +0200

    Accepted the Server parameters into variables

---
+++ src/UDP Client/udpTcpParent.cpp
@@ -0,0 +1,44 @@
+/****************************************
+ * Yossi Silberhaft						*
+ * Exercise 4							*
+ * File: udpTcpParent.cpp				*
+ ****************************************/
+
+#include "udpTcpParent.h"
+
+
+/************************************************************************
+ * This is the udpTcpParent constructor                                 *
+************************************************************************/
+udpTcpParent::udpTcpParent() { }
+
+
+/************************************************************************
+ * This is the udpTcpParent Destructor                            *
+************************************************************************/
+udpTcpParent::~udpTcpParent() { }
+
+
+/************************************************************************
+ * This function will receive a socket number and will create a new     *
+ * socket with the number received                                      *
+************************************************************************/
+void udpTcpParent::newSocket(int sock_type) {
+    //socket() will create a socket and return a descriptor
+    int try_socket = socket(AF_INET, sock_type, 0);
+
+    if(try_socket < 0){
+        perror("Error creating Socket\n");
+    }
+    else {
+        sock = try_socket;
+    }
+
+}
+
+/************************************************************************
+ * This function will close down the connection that was made           *
+************************************************************************/
+void udpTcpParent::closeConnection() {
+    close(sock);
+}
