b3f31d7e575d50e47a1b4c48c06f538a0e792033
yossisilb <yossisilb@gmail.com>
2015-12-15 14:44:40 +0200

    Initial Commit

---
+++ src/UDP Client/TCP.cpp
@@ -0,0 +1,44 @@
+#include "TCP.h"
+
+/************************************************************************
+ * This is the TCP Class constructor		        					*
+************************************************************************/
+TCP::TCP() { }
+
+
+/************************************************************************
+	 * This is the TCP destructor      		        					*
+************************************************************************/
+TCP::~TCP() { }
+
+
+
+void TCP::sendTCP(char *data, unsigned int data_len) {
+    int sent_bytes = send(sock, data, data_len, 0);
+
+    if(sent_bytes < 0){
+        perror("Sent bytes < 0\n");
+    }
+
+}
+
+void TCP::reveiveTCP() {
+    char buffer[BUFFER_SIZE];
+    int expected_data_len = sizeof(buffer);
+
+    //Puts received bytes into buffer and returns size of received data
+    int read_bytes = recv(sock, buffer, expected_data_len, 0);
+
+    //if no bytes connection closed
+    if (read_bytes == 0) {
+        cout << "connection is closed\n";
+    }
+    //error bytes < 0
+    else if (read_bytes < 0) {
+        cout << "error. not enough bytes read";
+    }
+        //prints recieved buffer
+    else {
+        cout << "Received data : " << buffer << endl;
+    }
+}
