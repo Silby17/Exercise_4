f38b9472ddc246ca92a83d46608d7fc685175210
yossisilb <yossisilb@gmail.com>
2015-12-15 19:09:41 +0200

    Commented And sorted

--- src/main.cpp
+++ src/main.cpp
@@ -10,19 +10,17 @@
 
 using namespace std;
 
-#define IP_ADDRESS "127.0.0.1"
-
 /************************************************************************
-* 								*
+* This is the main function of the program that will create a			*
+ * new Cinema. It will then pass the given arguments of Server/Client	*
+ * type and port number to the run function								*
  ************************************************************************/
 int main(int argc, char **argv){
 	//Gets the Server/Client type and port number from Arguments
 	int port = atoi(argv[2]);
 	string type = argv[1];
 	Cinema cinema;
-
 	cinema.runCinema(type, port);
+
 	return 0;
-
-
 }
