92a0d63fe7bfdd5ff3ce8da6a74af6808ebd24f8
yossisilb <yossisilb@gmail.com>
2015-12-15 11:30:05 +0200

    added the reading of arguments from main and sending to run function

--- src/main.cpp
+++ src/main.cpp
@@ -7,42 +7,21 @@
 #include <list>
 #include <iostream>
 #include "Cinema.h"
-#include "UDPServer.h"
 
 using namespace std;
 
 #define IP_ADDRESS "127.0.0.1"
-#define SERVER_PORT 5678
 
 /************************************************************************
 * 								*
  ************************************************************************/
 int main(int argc, char **argv){
-	cout << "Starting Program" << endl;
-	//Gets the type and port number from giver parameters
-	//string type = argv[1];
-	//string port = argv[2];
-	//TODO remove the outputs below
-	//cout << "Type: " << type << endl;
-	//cout << "Port: " << port << endl;
-
-
-	char data[] = "Do you want to build a snowman?";
-	//the size of string
-	int data_len = sizeof(data);
-
-	UDPServer* server = new UDPServer;
-
-	server->newSocket(SOCK_DGRAM);
-	server->bindUDP(SERVER_PORT);
-	server->receiveFrom();
-
-
-	cout << "HERE" << endl;
-
-
+	//Gets the Server/Client type and port number from Arguments
+	int port = atoi(argv[2]);
+	string type = argv[1];
 	Cinema cinema;
-	cinema.runCinema();
+
+	cinema.runCinema(type, port);
 	return 0;
 
 
